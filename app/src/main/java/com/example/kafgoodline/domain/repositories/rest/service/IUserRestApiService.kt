6bf6cb817c95991e41243dce8d9621871a8a0f1e
HotsHom <46223801+HotsHom@users.noreply.github.com>
2020-03-26 21:43:12 +0700

    Create IUserRestApiService.kt

---
+++ app/src/main/java/com/example/kafgoodline/domain/repositories/rest/service/IUserRestApiService.kt
@@ -0,0 +1,36 @@
+package com.example.kafgoodline.domain.repositories.rest.service
+
+import retrofit2.http.Body
+import retrofit2.http.PUT
+import io.reactivex.Observable
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.Call
+import com.example.kafgoodline.domain.di.models.Token
+import com.example.kafgoodline.domain.di.models.User
+
+interface IUserRestApiService {
+
+
+    /**
+     * Регистрация нового профиля пользователя
+     */
+    @POST("/auth/users/")
+    fun registration(@Body user: User): Observable<User>
+
+
+    /**
+     * Авторизация пользователя по существующему профилю
+     */
+    @POST("/auth/jwt/create/")
+    fun login(@Body user: User): Observable<User>
+
+
+    /**
+     * Будет использовать для обновления текущего токена пользователя
+     */
+    @POST("/auth/jwt/refresh/")
+    fun refreshToken(
+        @Body refreshToken: String?
+    ): Call<User>
+}
