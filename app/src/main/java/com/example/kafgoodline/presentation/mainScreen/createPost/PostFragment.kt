ac99db968849fbf5fc1688b36baecd3148be8a11
HotsHom <46223801+HotsHom@users.noreply.github.com>
2020-04-09 18:45:52 +0700

    Create Post Screen Design

---
+++ app/src/main/java/com/example/kafgoodline/presentation/mainScreen/createPost/PostFragment.kt
@@ -0,0 +1,24 @@
+package com.example.kafgoodline.presentation.mainScreen.createPost
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.kafgoodline.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false)
+    }
+
+}
